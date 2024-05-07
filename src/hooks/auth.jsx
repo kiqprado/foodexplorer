749908bba2ad17c0193f636d7c1c3fc2ad472f9e
Kaique Prado <kiqprado@outlook.com>
2024-05-07 11:57:01 -0300

    feat: Login, LogOut and LocalStorage settings

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,18 +1,21 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import { api } from '../services/api'
 
 export const AuthContext = createContext({})
 
-function AuthProvider({ Children }) {
+function AuthProvider({ children }) {
   const [ data, setData ] = useState({})
 
   async function signIn({ email, password}) {
     try {
-      const response =  await api.post("/sessions", { email, password})
+      const response =  await api.post('/sessions', { email, password})
       const { user, token } = response.data
 
       api.defaults.headers.authorization = `Bearer ${token}`
       setData({ user, token})
+
+      localStorage.setItem('@foodexplorer:user', JSON.stringify(user))
+      localStorage.setItem('@foodexplorer:token', token)
 
     } catch(error) {
       if(error.response){
@@ -21,13 +24,34 @@ function AuthProvider({ Children }) {
         alert("Não foi possível realizar o login.")
       }
     }
-   
   }
 
+  function signOut(){
+    localStorage.removeItem('@foodexplorer:token')
+    localStorage.removeItem('@foodexplorer:user')
+
+    setData({})
+  }
+  
+  useEffect(() => {
+    const token = localStorage.getItem('@foodexplorer:token')
+    const user = localStorage.getItem('@foodexplorer:user')
+
+    if(token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  }, [])
+
   return (
-  <AuthContext.Provider value={{ signIn, data: data.user }}>
-    {Children}
-  </AuthContext.Provider>)
+    <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
 function useAuth() {
