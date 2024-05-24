5f930df31b2031478e321aa08dd28b62bf859dad
Kaique Prado <kiqprado@outlook.com>
2024-05-24 09:38:16 -0300

    fix: fixing a issue on dish page

--- src/pages/Dish/index.jsx
+++ src/pages/Dish/index.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import { api } from '../../services/api'
 import { Link } from 'react-router-dom'
 
 import { Container } from './stytles'
-import dish from '../../assets/dish.png'
 import { Header } from '../../components/Header'
 
-export function Dish({data}) {
+import dishPlaceholder from '../../assets/dishPlaceholder.svg'
+
+export function Dish({ data, ...rest}) {
+  const dishAvatar = data.avatar ? `${api.defaults.baseURL}/files/${data.avatar}` : dishPlaceholder
+
   return(
-    <Container>
+    <Container {...rest}>
       <Header/>
 
       <main>
@@ -16,7 +19,7 @@ export function Dish({data}) {
           &lt; voltar
         </Link>
 
-        <img src={dish} alt="" />
+        <img src={dishAvatar} alt={data.title} />
 
         <h1>{data.title}</h1>
 
