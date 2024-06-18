fb56f380de2bd981e6172e912e4ce8b1fe342f7f
Kaique Prado <kiqprado@outlook.com>
2024-06-18 17:50:56 -0300

    fix: fixing cards styles

--- src/components/CardsAD/styles.js
+++ src/components/CardsAD/styles.js
@@ -11,27 +11,23 @@ export const Card = styled.div`
 
   position: relative;
 
-  > img {
-    height: 14.3rem;
+  img {
     position: absolute;
     z-index: 1;
-    bottom: 0;
-    left: -2.4rem;
   }
 
   .about {
     display: flex;
     flex-direction: column;
-    margin-left: 15rem;
     gap: 0.6rem;
+  }
 
-    h3 {
-      font-size: 1.8rem;
-    }
+  h3 {
+    font-size: 1.8rem;
+  }
 
-    p {
-      font-size: 1.2rem;
-    }
+  p {
+    font-size: 1.2rem;
   }
 
   color: ${({ theme }) => theme.COLORS.LIGHT_300};
@@ -41,4 +37,71 @@ export const Card = styled.div`
     rgba(6, 23, 30, 1) 50%,
     rgba(8, 24, 30, 1) 100%
   );
+
+  &.one {
+    > img {
+      height: 14.3rem;
+      bottom: 0;
+      left: -2.4rem;
+    }
+
+    .about {
+      margin-left: 15rem;
+    }
+  }
+
+  &.two {
+    > img {
+      width: 22.8rem;
+      bottom: 0rem;
+      right: -1.4rem;
+    }
+
+    .about {
+      margin-left: 1.6rem;
+      
+
+      text-align: center;
+    }
+
+    p {
+      max-width: 12rem;
+    }
+  }
+
+  &.three {
+    > img {
+      width: 16rem;
+      bottom: 0;
+      left: 12rem;
+    }
+
+    .about {
+      flex-direction: row;
+      text-align: center;
+      align-items: center;
+      gap: 16rem;
+
+      h3 {
+        margin-left: 1.6rem;
+        max-width: 10rem;
+      }
+
+      p {
+        max-width: 6rem;
+      }
+    }
+  }
+
+  &.four {
+    > img {
+      height: 12.3rem;
+      bottom: 0;
+      right: .6rem;
+    }
+
+    .about {
+      margin-left: 1.6rem;
+    }
+  }
 `
